8f3cd18266ac50d86f0d7eeef5e2e3957081a7d0
Gracious Ogyiri Asare <gpoa@bu.edu>
2024-06-20 22:57:26 +0000

    timezon maps api

--- map_app.py
+++ map_app.py
@@ -1,50 +1,18 @@
 import requests
 import json
 import googlemaps
+import os
+import pprint
 
-key = 'AIzaSyCzMX7Dl4ks60vKUms_pQj5EYjBhyYfj4E'
-AUTH_URL = 'https://routes.googleapis.com/distanceMatrix/v2:computeRouteMatrix'
-payload = {
-  "origin":{
-    "location":{
-      "latLng":{
-        "latitude": 37.419734,
-        "longitude": -122.0827784
-      }
-    }
-  },
-  "destination":{
-    "location":{
-      "latLng":{
-        "latitude": 37.417670,
-        "longitude": -122.079595
-      }
-    }
-  },
-  "travelMode": "DRIVE",
-  "routingPreference": "TRAFFIC_AWARE",
-  "departureTime": "2023-10-15T15:01:23.045123456Z",
-  "computeAlternativeRoutes": False,
-  "routeModifiers": {
-    "avoidTolls": False,
-    "avoidHighways": False,
-    "avoidFerries": False
-  },
-  "languageCode": "en-US",
-  "units": "IMPERIAL"
-}
+AUTH_URL = 'https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810%2C-119.6822510&timestamp=1331161200&key=?'
 
-headers = {
-  'Content-Type': 'application/json',
-  'X-Goog-Api-Key': key,
-  'X-Goog-FieldMask': 'routes.duration, routes.distanceMeters, routes.polyline.encodedPolyline'
-}
+# headers = {
+#   'Content-Type': 'application/json',
+#   'X-Goog-Api-Key': key,
+#   'X-Goog-FieldMask': 'routes.duration, routes.distanceMeters, routes.polyline.encodedPolyline'
+# }
 
-auth_response = requests.post(AUTH_URL, headers=headers, data=json.dumps(payload))
-
-
+auth_response = requests.get(AUTH_URL)
 # r = requests.get()
-
-
-print(auth_response)
+pprint.pprint(auth_response.json())
 
