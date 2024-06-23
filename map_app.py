54d9ff3893edfa7c9d1b177aa3c3672aad9ca5ac
Gracious O. Asare <123197195+sheisgracious@users.noreply.github.com>
2024-06-23 00:23:10 -0500

    shortened long line of code

--- map_app.py
+++ map_app.py
@@ -4,7 +4,9 @@ import googlemaps
 import os
 import pprint
 
-AUTH_URL = 'https://maps.googleapis.com/maps/api/timezone/json?location=39.6034810%2C-119.6822510&timestamp=1331161200&key=?'
+AUTH_URL = 'https://maps.googleapis.com/maps/'\
+          'api/timezone/json?location=39.6034810%'\
+          '2C-119.6822510&timestamp=1331161200&key=?'\
 
 # headers = {
 #   'Content-Type': 'application/json',
@@ -15,4 +17,3 @@ AUTH_URL = 'https://maps.googleapis.com/maps/api/timezone/json?location=39.60348
 auth_response = requests.get(AUTH_URL)
 # r = requests.get()
 pprint.pprint(auth_response.json())
-
